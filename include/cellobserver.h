5f01b4d35b613ecefa0a68cbf5b5d5be572b0b48
Christopher Mauney <mauneyc@pn2305185.lanl.gov>
2023-10-09 10:33:08 -0600

    source changes for compiliation

--- include/cellobserver.h
+++ include/cellobserver.h
@@ -1,35 +1,34 @@
 /*© 2023. Triad National Security, LLC. All rights reserved.
-This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
-National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
-Department of Energy/National Nuclear Security Administration. All rights in the program are.
-reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
-Security Administration. The Government is granted for itself and others acting on its behalf a
-nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare.
-derivative works, distribute copies to the public, perform publicly and display publicly, and to permit.
-others to do so.*/
+This program was produced under U.S. Government contract 89233218CNA000001 for
+Los Alamos National Laboratory (LANL), which is operated by Triad National
+Security, LLC for the U.S. Department of Energy/National Nuclear Security
+Administration. All rights in the program are. reserved by Triad National
+Security, LLC, and the U.S. Department of Energy/National Nuclear Security
+Administration. The Government is granted for itself and others acting on its
+behalf a nonexclusive, paid-up, irrevocable worldwide license in this material
+to reproduce, prepare. derivative works, distribute copies to the public,
+perform publicly and display publicly, and to permit. others to do so.*/
 
 #pragma once
 
-#include "H5Cpp.h"
+// #include "H5Cpp.h"
 #include "cell.h"
 
 #include <vector>
 
-class CellObserver
-{
+class CellObserver {
   std::size_t cid;
-  
+
   uint32_t num_nuc;
   uint32_t num_spec;
   uint32_t numBins;
   std::string modNum;
-  std::string   ofname;
-  std::string   RSname;
+  std::string ofname;
+  std::string RSname;
 
-  std::string    grnNames="";
+  std::string grnNames = "";
 
 private:
-
   cell_state m_state;
   double m_time;
   double m_elapsed;
@@ -42,11 +41,11 @@ private:
   std::string hfname;
 
 public:
-  CellObserver(std::size_t cid, const network* net, configuration* con);
+  CellObserver(std::size_t cid, const network *net, configuration *con);
   virtual ~CellObserver() {}
-  void init_dump(const cell_state& s);
-  void operator()(const cell_state& s);
-  void dump_data(const cell_state& s);
-  void restart_dump(const cell_state& s);
-  void finalSave(const cell_state& s);
+  void init_dump(const cell_state &s);
+  void operator()(const cell_state &s);
+  void dump_data(const cell_state &s);
+  void restart_dump(const cell_state &s);
+  void finalSave(const cell_state &s);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,33 @@
 /*© 2023. Triad National Security, LLC. All rights reserved.
-This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
-National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
-Department of Energy/National Nuclear Security Administration. All rights in the program are.
-reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
-Security Administration. The Government is granted for itself and others acting on its behalf a
-nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare.
-derivative works, distribute copies to the public, perform publicly and display publicly, and to permit.
-others to do so.*/
+This program was produced under U.S. Government contract 89233218CNA000001 for
+Los Alamos National Laboratory (LANL), which is operated by Triad National
+Security, LLC for the U.S. Department of Energy/National Nuclear Security
+Administration. All rights in the program are. reserved by Triad National
+Security, LLC, and the U.S. Department of Energy/National Nuclear Security
+Administration. The Government is granted for itself and others acting on its
+behalf a nonexclusive, paid-up, irrevocable worldwide license in this material
+to reproduce, prepare. derivative works, distribute copies to the public,
+perform publicly and display publicly, and to permit. others to do so.*/
 
-#include "nudust.h"
-#include "logging.h"
-#include "timer.h"
 #include "configuration.h"
-
+#include "logging.h"
+#include "nudust.h"
+#include "timer.h"
 
 #include <plog/Initializers/RollingFileInitializer.h>
 #include <plog/Log.h>
 #include <string>
 #include <vector>
+#ifdef NUDUSTC_ENABLE_MPI
 #include <mpi.h>
+#endif
+
 // TODO: (maybe) this should be it's own module...not a lot done tho
 #include <boost/program_options.hpp>
 #include <boost/program_options/options_description.hpp>
 
 // TODO: move these elsewhere
-inline void
-banner()
-{
+inline void banner() {
   std::cout << R"(
        +                                :
  .              .      `    |   o       !  '
@@ -50,16 +51,14 @@ and chemistry.
 )";
 }
 
-int
-main(int argc, char* argv[])
-{
-
+int main(int argc, char *argv[]) {
+  int rank = 0, size = 1;
+#ifdef NUDUSTC_ENABLE_MPI
   MPI_Init(&argc, &argv);
 
-  int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+#endif
   namespace po = boost::program_options;
 
   // setup command-line options
@@ -68,12 +67,11 @@ main(int argc, char* argv[])
 
   po::options_description desc("nuDust options");
   desc.add_options()("help", "print help message")(
-    "config_file,c",
-    po::value<std::string>(&config_filename),
-    "filename with runtime parameters")(
-    "log_file,l",
-    po::value<std::string>(&log_filename)->default_value("log.txt"),
-    "filename of log");
+      "config_file,c", po::value<std::string>(&config_filename),
+      "filename with runtime parameters")(
+      "log_file,l",
+      po::value<std::string>(&log_filename)->default_value("log.txt"),
+      "filename of log");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -97,8 +95,7 @@ main(int argc, char* argv[])
   std ::cout << "! configuration file = " << config_filename << "\n";
   std ::cout << "starting ...\n";
 
-  if(rank == 0) 
-  {
+  if (rank == 0) {
     banner();
 
     std ::cout << "\n";
@@ -106,15 +103,16 @@ main(int argc, char* argv[])
     std ::cout << "! configuration file = " << config_filename << "\n";
     std ::cout << "! pe = " << size << "\n";
   }
+#ifdef NUDUSTC_ENABLE_MPI
   MPI_Barrier(MPI_COMM_WORLD);
+#endif
 
   plog::init(plog::debug, log_filename.c_str());
   plog::init<DetailLog_Root>(plog::info, &rootAppender);
   plog::init<DetailLog_Rank>(plog::info, &rankAppender);
 
-  
-  //SCLOG_0(rank) << "nuDust has started";
-  //SCLOG_A(rank) << "starting ...\n";
+  // SCLOG_0(rank) << "nuDust has started";
+  // SCLOG_A(rank) << "starting ...\n";
   std::cout << "! nuDust has started\n";
 
   PLOGI << "nuDust has started";
@@ -123,14 +121,15 @@ main(int argc, char* argv[])
   nd.run();
 
 #ifdef ENABLE_BENCHMARK
-  for(const auto& [k, v] : benchmark_map)
-{
-  auto call_avg = std::accumulate(v.begin(), v.end(), 0) / v.size();
-  SCLOG_A(rank) << "fn = " << k << " is called " << v.size() <<" times: avg/call = " << call_avg <<" us\n";
-}
+  for (const auto &[k, v] : benchmark_map) {
+    auto call_avg = std::accumulate(v.begin(), v.end(), 0) / v.size();
+    SCLOG_A(rank) << "fn = " << k << " is called " << v.size()
+                  << " times: avg/call = " << call_avg << " us\n";
+  }
 #endif
 
+#ifdef NUDUSTC_ENABLE_MPI
   MPI_Finalize();
+#endif
   return 0;
 }
-
